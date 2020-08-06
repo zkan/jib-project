ba7a2646e90e0f4e8029b1dd5ca656b47d10082b
zkan <zkan.cs@gmail.com>
2020-08-06 15:40:33 +0700

    Use template :bear:

--- safety/workers/views.py
+++ safety/workers/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.views import View
 
 from .models import Worker
@@ -9,8 +10,15 @@ class WorkerListView(View):
     def get(self, request):
         workers = Worker.objects.all()
         print(workers)
-        html = ''
-        for worker in workers:
-            html += f'<li>{worker.first_name}</li>'
 
-        return HttpResponse(html)
+        # html = ''
+        # for worker in workers:
+        #     html += f'<li>{worker.first_name}</li>'
+
+        worker_list = []
+        for worker in workers:
+            worker_list.append(worker.first_name)
+
+        return render(request, 'worker_list.html', {
+            'workers': worker_list
+        })
