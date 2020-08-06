57e275321d9a7f6f766f2f1d25d0bc59b8937e2b
zkan <zkan.cs@gmail.com>
2020-08-06 12:31:00 +0700

    Add worker model :bear:

--- safety/workers/models.py
+++ safety/workers/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Worker(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=40)
+    is_available = models.BooleanField()
+    primary_phone = models.CharField(max_length=10)
+    secondary_phone = models.CharField(max_length=10)
+    address = models.TextField()
--- safety/workers/tests.py
+++ safety/workers/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Worker
+
+
+class TestWorker(TestCase):
+    def test_worker_should_have_defined_fields(self):
+        # Given
+        first_name = 'Keng'
+        last_name = 'Mak'
+        is_available = True
+        primary_phone = '081-689-777x'
+        secondary_phone = '081-689-778x'
+        address = 'Geeky Base All Stars'
+
+        # When
+        worker = Worker.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            is_available=is_available,
+            primary_phone=primary_phone,
+            secondary_phone=secondary_phone,
+            address=address,
+        )
+
+        # Then
+        self.assertEqual(worker.first_name, first_name)
+        self.assertEqual(worker.last_name, last_name)
+        self.assertTrue(worker.is_available)
+        self.assertEqual(worker.primary_phone, primary_phone)
+        self.assertEqual(worker.secondary_phone, secondary_phone)
+        self.assertEqual(worker.address, address)
