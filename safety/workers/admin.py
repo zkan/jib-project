fcd777819e2324339f9fc73ca4ecb94fa43e9870
zkan <zkan.cs@gmail.com>
2020-08-06 14:23:12 +0700

    Register worker model to admin :bear:

--- safety/workers/admin.py
+++ safety/workers/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Worker
+
+
+@admin.register(Worker)
+class WorkerAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+        'last_name',
+        'is_available',
+        'primary_phone',
+        'secondary_phone',
+    )
+    list_filter = (
+        'is_available',
+    )
