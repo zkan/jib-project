33827aa2be4cddb864c0b26fe428d218fa460aaa
zkan <zkan.cs@gmail.com>
2020-08-06 15:23:08 +0700

    Render workers :bear:

--- safety/workers/tests/test_views.py
+++ safety/workers/tests/test_views.py
@@ -1,8 +1,37 @@
 from django.test import TestCase
 
+from ..models import Worker
+
 
 class TestWorkerListView(TestCase):
     def test_view_should_be_accessible(self):
         response = self.client.get('/workers/')
         # print(dir(response))
         self.assertEqual(response.status_code, 200)
+
+    def test_view_should_render_list_of_worker_names(self):
+        # Given
+        Worker.objects.create(
+            first_name='Kan',
+            last_name='Ouivirach',
+            is_available=True,
+            primary_phone='1233451111',
+            secondary_phone='19239201',
+            address='Geeky Base',
+        )
+        Worker.objects.create(
+            first_name='Keng',
+            last_name='Mak',
+            is_available=False,
+            primary_phone='123315111',
+            secondary_phone='1923669201',
+            address='Oddstria',
+        )
+
+        # When
+        response = self.client.get('/workers/')
+
+        # Then
+        self.assertContains(response, '<li>Kan</li>')
+        self.assertContains(response, '<li>Keng</li>')
+        
--- safety/workers/views.py
+++ safety/workers/views.py
@@ -1,8 +1,16 @@
 from django.http import HttpResponse
 from django.views import View
 
+from .models import Worker
+
 
 # class-based view
 class WorkerListView(View):
     def get(self, request):
-        return HttpResponse()
+        workers = Worker.objects.all()
+        print(workers)
+        html = ''
+        for worker in workers:
+            html += f'<li>{worker.first_name}</li>'
+
+        return HttpResponse(html)
