7587d2fb9f38f831f881b10cd4bbfe54236afbdc
zkan <zkan.cs@gmail.com>
2020-08-08 11:20:46 +0700

    Call COVID-19 API :bear:

--- safety/covid19_reports/tests/test_views.py
+++ safety/covid19_reports/tests/test_views.py
@@ -1,7 +1,17 @@
+from unittest.mock import patch
+
 from django.test import TestCase
 
 
 class TestCovid19ReportView(TestCase):
-    def test_view_should_be_accessible(self):
+    @patch('covid19_reports.views.requests.get')
+    def test_view_should_be_accessible(self, _):
         response = self.client.get('/covid19-reports/')
         self.assertEqual(response.status_code, 200)
+
+    @patch('covid19_reports.views.requests.get')
+    def test_view_should_call_covid19_api(self, mock):
+        self.client.get('/covid19-reports/')
+        mock.assert_called_once_with(
+            'https://covid19.th-stat.com/api/open/today'
+        )
--- safety/covid19_reports/views.py
+++ safety/covid19_reports/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
 from django.views import View
 
+import requests
+
 
 class Covid19ReportView(View):
     def get(self, request):
+        requests.get('https://covid19.th-stat.com/api/open/today')
         return HttpResponse()
