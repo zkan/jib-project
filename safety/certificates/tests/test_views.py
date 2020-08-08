e1bbaf4842619bc2970b39c89016b063cabf715e
zkan <zkan.cs@gmail.com>
2020-08-08 15:55:02 +0700

    Create certificate model view set using DRF :bear:

---
+++ safety/certificates/tests/test_views.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+
+from ..models import Certificate
+from ..serializers import CertificateModelSerializer
+from ..views import CertificateModelViewSetView
+
+
+class TestCertificateModelViewSetView(TestCase):
+    def test_model_view_set_should_set_queryset(self):
+        assert list(CertificateModelViewSetView.queryset) == list(Certificate.objects.all())
+
+    def test_model_view_set_should_set_serializer_class(self):
+        assert CertificateModelViewSetView.serializer_class == CertificateModelSerializer
--- safety/certificates/views.py
+++ safety/certificates/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Certificate
+from .serializers import CertificateModelSerializer
+
+
+class CertificateModelViewSetView(ModelViewSet):
+    queryset = Certificate.objects.all()
+    serializer_class = CertificateModelSerializer
