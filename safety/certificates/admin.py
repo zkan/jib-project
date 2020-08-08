2e450226b65cc13aea0ec7893cc025ece18de5cc
zkan <zkan.cs@gmail.com>
2020-08-08 15:09:35 +0700

    Register certificate to admin :bear:

--- safety/certificates/admin.py
+++ safety/certificates/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Certificate
+
+
+@admin.register(Certificate)
+class CertificateAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'issued_by',
+    )
---
+++ safety/certificates/tests/test_admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.test import TestCase
+
+from ..admin import CertificateAdmin
+from ..models import Certificate
+
+
+class CertificateAdminTest(TestCase):
+    def test_admin_should_be_registered(self):
+        self.assertTrue(isinstance(
+            admin.site._registry[Certificate], 
+            CertificateAdmin)
+        )
+
+    def test_admin_should_set_list_display(self):
+        expected = (
+            'name',
+            'issued_by',
+        )
+        self.assertEqual(CertificateAdmin.list_display, expected)
