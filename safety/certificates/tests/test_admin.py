6c0d605d8ce2e038d9138df091d6e13e6693aa2e
zkan <zkan.cs@gmail.com>
2020-08-08 15:13:13 +0700

    Use assert from pytest :bear:

--- safety/certificates/tests/test_admin.py
+++ safety/certificates/tests/test_admin.py
@@ -7,14 +7,12 @@ from ..models import Certificate
 
 class CertificateAdminTest(TestCase):
     def test_admin_should_be_registered(self):
-        self.assertTrue(isinstance(
-            admin.site._registry[Certificate], 
-            CertificateAdmin)
-        )
+        assert isinstance(admin.site._registry[Certificate], 
+                          CertificateAdmin) is True
 
     def test_admin_should_set_list_display(self):
         expected = (
             'name',
             'issued_by',
         )
-        self.assertEqual(CertificateAdmin.list_display, expected)
+        assert CertificateAdmin.list_display == expected
