cff52d67acce0631875530f9df7968876b8326ed
zkan <zkan.cs@gmail.com>
2020-08-08 16:40:24 +0700

    Add worker as foreign key :bear:

--- safety/certificates/models.py
+++ safety/certificates/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
+from workers.models import Worker
+
 
 class Certificate(models.Model):
     name = models.CharField(max_length=100)
     issued_by = models.CharField(max_length=50)
+    worker = models.ForeignKey(Worker, on_delete=models.CASCADE)
--- safety/certificates/tests/test_models.py
+++ safety/certificates/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 from ..models import Certificate
+from workers.models import Worker
 
 
 class TestCertificate(TestCase):
@@ -11,13 +12,24 @@ class TestCertificate(TestCase):
         pass
 
     def test_certificate_should_have_defined_fields(self):
+        worker = Worker.objects.create(
+            first_name='Kan',
+            last_name='Ouivirach',
+            is_available=True,
+            primary_phone='1233451111',
+            secondary_phone='19239201',
+            address='Geeky Base',
+        )
+
         name = 'Django Certificate by ODDS'
         issued_by = 'ProoF'
 
         certificate = Certificate.objects.create(
             name=name,
             issued_by=issued_by,
+            worker=worker,
         )
 
         assert certificate.name == name
         assert certificate.issued_by == issued_by
+        assert certificate.worker.first_name == worker.first_name
